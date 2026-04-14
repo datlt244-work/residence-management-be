ddf5eb9b0e67a6d56dd4feb068a0d69a230d8d6e
datlt1 <datlt1@newwave.com.vn>
2026-04-14 17:04:48 +0700

    Update BootstrapApplicationTests to configure H2 in-memory database for testing; set properties for datasource and Hibernate dialect.

--- bootstrap/src/test/java/com/base/bootstrap/BootstrapApplicationTests.java
+++ bootstrap/src/test/java/com/base/bootstrap/BootstrapApplicationTests.java
@@ -4,7 +4,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(
+		properties = {
+				"spring.flyway.enabled=false",
+				"spring.datasource.url=jdbc:h2:mem:residence_test;MODE=MySQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+				"spring.datasource.driver-class-name=org.h2.Driver",
+				"spring.datasource.username=sa",
+				"spring.datasource.password=",
+				"spring.jpa.hibernate.ddl-auto=none",
+				"spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect"
+		}
+)
 @ActiveProfiles("test")
 class BootstrapApplicationTests {
 
